dabb371fd8f8cd9d46fea642f0ff6422b4f26c12
Harshit <harshit.ravi18@gmail.com>
2026-04-16 14:30:48 +0200

    feat: export EventForm component from index file

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/data-grid/DataGrid';
 export * from './lib/language-switcher/LanguageSwitcher';
 export * from './i18n/i18n';
 export * from './lib/timeline';
+export * from './lib/event-form';
